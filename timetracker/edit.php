a773553038cd5560a5c3b865feaf0e2b3eb679b0
nessatuncay <144923042+nessatuncay@users.noreply.github.com>
2026-04-20 20:20:27 -0400

    so i added the if function for updating the data

--- timetracker/edit.php
+++ timetracker/edit.php
@@ -11,4 +11,38 @@ if (!isset($_GET['id']))
 
 $taskId = $_GET['id'];
 
+/*this makes sure that if the form is submitted then the row will be updated*/ 
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+    {
+        /*this is to sanitize the data */
+        $taskName  = trim($_POST['task_name'] ?? '');
+        $category  = trim($_POST['category'] ?? '');
+        $dueDate   = trim($_POST['due_date'] ?? '');
+        $timeSpent = (int)($_POST['time_spent'] ?? 0);
 
+
+        /*this is validating the info in a simple */
+        if ($taskName === '' || $category === '' || $dueDate === '')
+            {
+                /*if task name, category and due date is emoty then it gives this error */
+                $error = "The task name, category and due date is required";
+            }
+        else
+            {
+                /*this updates the data in the database */
+                $sql = "UPDATE tasks SET task_name = :task_name, category = :category, due date = :due_date, time_spent = :time_spent WHERE id = :id";
+
+                $stmt = $pdo->prepare($sql);
+
+                /*this binds the parameters*/
+                $stmt->bindParam(':task_name', $taskName);
+                $stmt->bindParam(':category', $category);
+                $stmt->bindParam(':due_date', $dueDate);
+                $stmt->bindParam(':time_spent', $timeSpent);
+                $stmt->bindParam(':id', $taskId);
+
+                $stmt->execute();
+
+                exit;
+            }
+    }
